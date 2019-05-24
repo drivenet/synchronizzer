c60178067dc637581a252f870b644e1e335503b2
Aristarkh Zagorodnikov <xm@drive.net>
2019-05-24 12:34:09 +0300

    Tune publish

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,6 +1,6 @@
 @echo off
 rmdir /s /q gridfs-sync-service\bin\x64\Release\netcoreapp2.2\linux-x64\publish
-dotnet publish -c Release -r linux-x64
+dotnet publish -c Release -r linux-x64 --self-contained false
 rmdir /s /q packages\gridfs-sync-service-linux-x64
 mkdir packages\gridfs-sync-service-linux-x64
 xcopy gridfs-sync-service\bin\x64\Release\netcoreapp2.2\linux-x64\publish\*.dll packages\gridfs-sync-service-linux-x64
