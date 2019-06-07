aeb165381ff76533e3717253e31b686d3968d71e
Aristarkh Zagorodnikov <xm@drive.net>
2019-06-07 18:58:03 +0300

    More secure FilesystemObjectReader file access

--- gridfs-sync-service/Implementation/FilesystemObjectReader.cs
+++ gridfs-sync-service/Implementation/FilesystemObjectReader.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -17,12 +18,18 @@ namespace GridFSSyncService.Implementation
         public async Task<Stream> Read(string objectName, CancellationToken cancellationToken)
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
-            if (Path.DirectorySeparatorChar != '/' && Path.AltDirectorySeparatorChar != '/')
+            if (Path.DirectorySeparatorChar != '/')
             {
                 objectName = objectName.Replace('/', Path.DirectorySeparatorChar);
             }
 
-            Stream stream = File.Open(_context.FilePath + Path.DirectorySeparatorChar + objectName, FileMode.Open, FileAccess.Read, FileShare.Read | FileShare.Delete);
+            var path = _context.FilePath + Path.DirectorySeparatorChar + objectName;
+            if (Path.GetFullPath(path) != path)
+            {
+                throw new ArgumentOutOfRangeException(nameof(path), path, FormattableString.Invariant($"The object name \"{objectName}\" produces an insecure path \"{path}\""));
+            }
+
+            Stream stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read | FileShare.Delete);
             return stream;
         }
     }
