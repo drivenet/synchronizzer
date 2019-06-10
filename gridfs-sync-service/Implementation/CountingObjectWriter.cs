6ce1968dc643bc177aaee207dd65d299e5d3f0c3
Aristarkh Zagorodnikov <xm@drive.net>
2019-06-10 18:19:37 +0300

    Improve object writer metric collection

--- gridfs-sync-service/Implementation/CountingObjectWriter.cs
+++ gridfs-sync-service/Implementation/CountingObjectWriter.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,25 +20,62 @@ namespace GridFSSyncService.Implementation
 
         public async Task Delete(string objectName, CancellationToken cancellationToken)
         {
-            _writer.Add("writer.delete.init", 1);
-            await _inner.Delete(objectName, cancellationToken);
-            _writer.Add("writer.delete.count", 1);
+            try
+            {
+                await _inner.Delete(objectName, cancellationToken);
+            }
+            catch (OperationCanceledException)
+            {
+                throw;
+            }
+            catch
+            {
+                _writer.Add("writer.delete_errors", 1);
+                throw;
+            }
+
+            _writer.Add("writer.deletes", 1);
         }
 
         public async Task Flush(CancellationToken cancellationToken)
         {
-            _writer.Add("writer.flush.init", 1);
-            await _inner.Flush(cancellationToken);
-            _writer.Add("writer.flush.count", 1);
+            try
+            {
+                await _inner.Flush(cancellationToken);
+            }
+            catch (OperationCanceledException)
+            {
+                throw;
+            }
+            catch
+            {
+                _writer.Add("writer.flush_errors", 1);
+                throw;
+            }
+
+            _writer.Add("writer.flushes", 1);
         }
 
         public async Task Upload(string objectName, Stream readOnlyInput, CancellationToken cancellationToken)
         {
-            _writer.Add("writer.upload.init", 1);
-            var length = readOnlyInput.Length;
-            await _inner.Upload(objectName, readOnlyInput, cancellationToken);
-            _writer.Add("writer.upload.count", 1);
-            _writer.Add("writer.upload.length", length);
+            long length;
+            try
+            {
+                length = readOnlyInput.Length;
+                await _inner.Upload(objectName, readOnlyInput, cancellationToken);
+            }
+            catch (OperationCanceledException)
+            {
+                throw;
+            }
+            catch
+            {
+                _writer.Add("writer.upload_errors", 1);
+                throw;
+            }
+
+            _writer.Add("writer.uploads", 1);
+            _writer.Add("writer.uploads_length", length);
         }
     }
 }
