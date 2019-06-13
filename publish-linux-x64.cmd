36540ccd9362191e5d03028a57f70d78386f2ba6
Aristarkh Zagorodnikov <xm@drive.net>
2019-06-13 19:02:08 +0300

    Simplify publish

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,9 +1,6 @@
 @echo off
-rmdir /s /q gridfs-sync-service\bin\x64\Release\netcoreapp2.2\linux-x64\publish
-dotnet publish -c Release -r linux-x64 --self-contained false
 rmdir /s /q packages\gridfs-sync-service-linux-x64
 mkdir packages\gridfs-sync-service-linux-x64
-xcopy gridfs-sync-service\bin\x64\Release\netcoreapp2.2\linux-x64\publish\*.dll packages\gridfs-sync-service-linux-x64
-xcopy gridfs-sync-service\bin\x64\Release\netcoreapp2.2\linux-x64\publish\*.pdb packages\gridfs-sync-service-linux-x64
-xcopy gridfs-sync-service\bin\x64\Release\netcoreapp2.2\linux-x64\publish\*.so packages\gridfs-sync-service-linux-x64
-xcopy gridfs-sync-service\bin\x64\Release\netcoreapp2.2\linux-x64\publish\*.runtimeconfig.json packages\gridfs-sync-service-linux-x64
+dotnet publish gridfs-sync-service --output ..\packages\gridfs-sync-service-linux-x64 -c Release -r linux-x64 --self-contained false
+rmdir /s /q packages\gridfs-sync-service-linux-x64\refs
+del packages\gridfs-sync-service-linux-x64\gridfs-sync-service packages\gridfs-sync-service-linux-x64\web.config packages\gridfs-sync-service-linux-x64\*.deps.json
