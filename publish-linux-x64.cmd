b55e5cf98718f3dbc05f8d85c157e0e79d94fda4
Aristarkh Zagorodnikov <xm@drive.net>
2019-06-18 13:06:18 +0300

    Update publish tool

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,6 +1,6 @@
 @echo off
-rmdir /s /q packages\gridfs-sync-service-linux-x64
-mkdir packages\gridfs-sync-service-linux-x64
-dotnet publish gridfs-sync-service --output ..\packages\gridfs-sync-service-linux-x64 -c Release -r linux-x64 --self-contained false
-rmdir /s /q packages\gridfs-sync-service-linux-x64\refs
-del packages\gridfs-sync-service-linux-x64\gridfs-sync-service packages\gridfs-sync-service-linux-x64\web.config packages\gridfs-sync-service-linux-x64\*.deps.json
+rmdir /s /q packages\linux-x64\gridfs-sync-service
+mkdir packages\linux-x64\gridfs-sync-service
+dotnet publish gridfs-sync-service --output ..\packages\linux-x64\gridfs-sync-service -c Release -r linux-x64 --self-contained false
+rmdir /s /q packages\linux-x64\gridfs-sync-service\refs
+del packages\linux-x64\gridfs-sync-service\gridfs-sync-service packages\linux-x64\gridfs-sync-service\web.config packages\linux-x64\gridfs-sync-service\*.deps.json
