ff28fbd3f6bc1cb6ba0d53132b4fecb65b0b0259
Aristarkh Zagorodnikov <xm@drive.net>
2019-06-19 12:11:29 +0300

    Fix GridFS iteration for non-unique file names

--- synchronizzer/Implementation/GridFSObjectSource.cs
+++ synchronizzer/Implementation/GridFSObjectSource.cs
@@ -12,7 +12,10 @@ namespace Synchronizzer.Implementation
 {
     internal sealed class GridFSObjectSource : IObjectSource
     {
-        private static readonly SortDefinition<GridFSFileInfo<BsonValue>> FilenameSort = Builders.Sort.Ascending(info => info.Filename);
+        private static readonly SortDefinition<GridFSFileInfo<BsonValue>> FilenameSort
+            = Builders.Sort
+                .Ascending(info => info.Filename)
+                .Ascending(info => info.UploadDateTime);
 
         private readonly GridFSContext _context;
 
@@ -37,8 +40,39 @@ namespace Synchronizzer.Implementation
             using (var infos = await _context.Bucket.FindAsync(filter, options, cancellationToken))
             {
                 await infos.ForEachAsync(
-                    info => result.Add(new ObjectInfo(info.Filename, info.Length)),
+                    info =>
+                    {
+                        var lastIndex = result.Count - 1;
+                        if (lastIndex >= 0
+                            && result[lastIndex].Name == info.Filename)
+                        {
+                            result.RemoveAt(lastIndex);
+                        }
+
+                        result.Add(new ObjectInfo(info.Filename, info.Length));
+                    },
                     cancellationToken);
+            }
+
+            var lastIndex = result.Count;
+            if (lastIndex == BatchSize)
+            {
+                --lastIndex;
+                var singleFilter = Builders.Filter.Eq(info => info.Filename, result[lastIndex].Name);
+                var singleOptions = new GridFSFindOptions<BsonValue>
+                {
+                    Sort = FilenameSort,
+                    Limit = 1,
+                };
+                using (var infos = await _context.Bucket.FindAsync(singleFilter, singleOptions, cancellationToken))
+                {
+                    result.RemoveAt(lastIndex);
+                    GridFSFileInfo<BsonValue>? info = await infos.SingleOrDefaultAsync(cancellationToken);
+                    if (info is object)
+                    {
+                        result.Add(new ObjectInfo(info.Filename, info.Length));
+                    }
+                }
             }
 
             return result;
