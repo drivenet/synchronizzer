9b43df7f78d9fa444fe95f77bdfca3cf22f28f4f
Aristarkh Zagorodnikov <xm@drive.net>
2019-06-25 23:57:14 +0300

    Fix parallel locking

--- synchronizzer/Implementation/S3ObjectWriter.cs
+++ synchronizzer/Implementation/S3ObjectWriter.cs
@@ -11,7 +11,9 @@ using Amazon.S3.Model;
 
 namespace Synchronizzer.Implementation
 {
+#pragma warning disable CA1001 // Types that own disposable fields should be disposable -- _lock does not need disposal because wait handle is never allocated
     internal sealed class S3ObjectWriter : IObjectWriter
+#pragma warning restore CA1001 // Types that own disposable fields should be disposable
     {
         private const string LockExtension = ".lock";
         private static readonly TimeSpan LockInterval = TimeSpan.FromSeconds(17);
@@ -20,6 +22,7 @@ namespace Synchronizzer.Implementation
         private readonly S3WriteContext? _recycleContext;
         private readonly string _lockName = Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture);
         private readonly Stopwatch _timer = new Stopwatch();
+        private readonly SemaphoreSlim _lock = new SemaphoreSlim(1);
 
         public S3ObjectWriter(S3WriteContext context, S3WriteContext? recycleContext)
         {
@@ -88,74 +91,26 @@ namespace Synchronizzer.Implementation
 
         public async Task Lock(CancellationToken cancellationToken)
         {
-            if (_timer.IsRunning
-                && _timer.Elapsed < LockInterval)
+            try
             {
-                return;
-            }
-
-            var nowTask = GetUtcTime(cancellationToken);
-            var tasks = new List<Task>();
-            var lockLifetime = TimeSpan.FromMinutes(5);
-            var listRequest = new ListObjectsV2Request
-            {
-                BucketName = _context.BucketName,
-                Prefix = S3Constants.LockPrefix,
-            };
-            while (true)
-            {
-                var listResponse = await _context.S3.ListObjectsV2Async(listRequest, cancellationToken);
-                var threshold = (await nowTask) - lockLifetime;
-                var locked = false;
-                foreach (var s3Object in listResponse.S3Objects)
+                await _lock.WaitAsync(cancellationToken);
+                if (!_timer.IsRunning
+                    || _timer.Elapsed > LockInterval)
                 {
-                    var key = s3Object.Key;
-                    var lockTime = s3Object.LastModified.ToUniversalTime();
-                    if (lockTime < threshold)
-                    {
-                        var deleteObjectRequest = new DeleteObjectRequest
-                        {
-                            BucketName = _context.BucketName,
-                            Key = key,
-                        };
-                        var deleteTask = _context.S3.DeleteObjectAsync(deleteObjectRequest, cancellationToken);
-                        tasks.Add(deleteTask);
-                    }
-                    else if (key.EndsWith(LockExtension, StringComparison.OrdinalIgnoreCase))
-                    {
-                        var keyData = key.Substring(S3Constants.LockPrefix.Length, key.Length - LockExtension.Length - S3Constants.LockPrefix.Length);
-                        var comparison = string.CompareOrdinal(keyData, _lockName);
-                        if (comparison < 0)
-                        {
-                            _timer.Stop();
-                            throw new OperationCanceledException(FormattableString.Invariant($"The lock \"{_lockName}\" was overriden by \"{keyData}\" ({lockTime:o})."));
-                        }
-
-                        if (comparison == 0)
-                        {
-                            locked = true;
-                            break;
-                        }
-                    }
-
-                    listRequest.StartAfter = s3Object.Key;
-                }
-
-                if (locked || !listResponse.IsTruncated)
-                {
-                    break;
+                    await LockCore(cancellationToken);
+                    _timer.Restart();
                 }
             }
-
-            var request = new PutObjectRequest
+            finally
             {
-                BucketName = _context.BucketName,
-                Key = S3Constants.LockPrefix + _lockName + LockExtension,
-            };
-            var putTask = _context.S3.PutObjectAsync(request, cancellationToken);
-            tasks.Add(putTask);
-            await Task.WhenAll(tasks);
-            _timer.Restart();
+                try
+                {
+                    _lock.Release();
+                }
+                catch (SemaphoreFullException)
+                {
+                }
+            }
         }
 
         public async Task Upload(string objectName, Stream readOnlyInput, CancellationToken cancellationToken)
@@ -230,6 +185,70 @@ namespace Synchronizzer.Implementation
                 {
                 }
             }
+        }
+
+        private async Task LockCore(CancellationToken cancellationToken)
+        {
+            var nowTask = GetUtcTime(cancellationToken);
+            var tasks = new List<Task>();
+            var lockLifetime = TimeSpan.FromMinutes(5);
+            var listRequest = new ListObjectsV2Request
+            {
+                BucketName = _context.BucketName,
+                Prefix = S3Constants.LockPrefix,
+            };
+            while (true)
+            {
+                var listResponse = await _context.S3.ListObjectsV2Async(listRequest, cancellationToken);
+                var threshold = (await nowTask) - lockLifetime;
+                var locked = false;
+                foreach (var s3Object in listResponse.S3Objects)
+                {
+                    var key = s3Object.Key;
+                    var lockTime = s3Object.LastModified.ToUniversalTime();
+                    if (lockTime < threshold)
+                    {
+                        var deleteObjectRequest = new DeleteObjectRequest
+                        {
+                            BucketName = _context.BucketName,
+                            Key = key,
+                        };
+                        var deleteTask = _context.S3.DeleteObjectAsync(deleteObjectRequest, cancellationToken);
+                        tasks.Add(deleteTask);
+                    }
+                    else if (key.EndsWith(LockExtension, StringComparison.OrdinalIgnoreCase))
+                    {
+                        var keyData = key.Substring(S3Constants.LockPrefix.Length, key.Length - LockExtension.Length - S3Constants.LockPrefix.Length);
+                        var comparison = string.CompareOrdinal(keyData, _lockName);
+                        if (comparison < 0)
+                        {
+                            throw new OperationCanceledException(FormattableString.Invariant($"The lock \"{_lockName}\" was overriden by \"{keyData}\" ({lockTime:o})."));
+                        }
+
+                        if (comparison == 0)
+                        {
+                            locked = true;
+                            break;
+                        }
+                    }
+
+                    listRequest.StartAfter = s3Object.Key;
+                }
+
+                if (locked || !listResponse.IsTruncated)
+                {
+                    break;
+                }
+            }
+
+            var request = new PutObjectRequest
+            {
+                BucketName = _context.BucketName,
+                Key = S3Constants.LockPrefix + _lockName + LockExtension,
+            };
+            var putTask = _context.S3.PutObjectAsync(request, cancellationToken);
+            tasks.Add(putTask);
+            await Task.WhenAll(tasks);
         }
     }
 }
