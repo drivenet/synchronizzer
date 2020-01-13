4f0b77606cdb66907383bab5a82fdf31490128c2
Aristarkh Zagorodnikov <xm@drive.net>
2020-01-13 16:20:44 +0300

    Cleanup publish script

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -3,4 +3,4 @@ rmdir /s /q packages\linux-x64\synchronizzer
 mkdir packages\linux-x64\synchronizzer
 dotnet publish synchronizzer --output packages\linux-x64\synchronizzer -c Release -r linux-x64 --self-contained false
 rmdir /s /q packages\linux-x64\synchronizzer\refs
-del packages\linux-x64\synchronizzer\synchronizzer packages\linux-x64\synchronizzer\web.config packages\linux-x64\synchronizzer\*.deps.json
+del packages\linux-x64\synchronizzer\synchronizzer packages\linux-x64\synchronizzer\*.deps.json
