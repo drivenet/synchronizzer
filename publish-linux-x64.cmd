0797be67cd8cd18d5ccc83097a45daf2c37c7ddc
Aristarkh Zagorodnikov <xm@drive.net>
2020-04-16 16:55:15 +0300

    Allow using binary for Linux package

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -2,4 +2,4 @@
 rmdir /s /q packages\linux-x64\synchronizzer
 mkdir packages\linux-x64\synchronizzer
 dotnet publish synchronizzer --output packages\linux-x64\synchronizzer -c Release -r linux-x64 --self-contained false
-del packages\linux-x64\synchronizzer\synchronizzer packages\linux-x64\synchronizzer\web.config packages\linux-x64\synchronizzer\*.deps.json
+del packages\linux-x64\synchronizzer\web.config packages\linux-x64\synchronizzer\*.deps.json
