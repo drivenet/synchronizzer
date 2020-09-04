79eb02085ea52b901e819b5cab0920e2e5fc7199
Aristarkh Zagorodnikov <xm@drive.net>
2020-09-04 22:40:29 +0300

    Fix publish configuration

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,7 +1,7 @@
 @echo off
 rmdir /s /q packages\linux-x64\synchronizzer
 mkdir packages\linux-x64\synchronizzer
-dotnet publish Synchronizzer --output packages\linux-x64\synchronizzer -c Release -r linux-x64 --self-contained false
+dotnet publish Synchronizzer --output packages\linux-x64\synchronizzer -c Integration -r linux-x64 --self-contained false
 move packages\linux-x64\synchronizzer\Microsoft.Extensions.Hosting.Systemd.dll "%TEMP%"
 del packages\linux-x64\synchronizzer\web.config packages\linux-x64\synchronizzer\*.deps.json packages\linux-x64\synchronizzer\Microsoft.*.dll
 move "%TEMP%\Microsoft.Extensions.Hosting.Systemd.dll" packages\linux-x64\synchronizzer
