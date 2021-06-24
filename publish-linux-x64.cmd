bfb74f6787e16356598bdf0371c603567692d5fe
Aristarkh Zagorodnikov <xm@drive.net>
2021-06-24 19:46:27 +0300

    Update build scripts

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,7 +1,7 @@
 @echo off
 rmdir /s /q packages\linux-x64\synchronizzer
 mkdir packages\linux-x64\synchronizzer
-dotnet publish Synchronizzer --output packages\linux-x64\synchronizzer -c Integration -r linux-x64 --self-contained false
+dotnet publish Synchronizzer --force --output packages\linux-x64\synchronizzer -c Integration -r linux-x64 --self-contained false
 move packages\linux-x64\synchronizzer\Microsoft.Extensions.Hosting.Systemd.dll "%TEMP%"
 move packages\linux-x64\synchronizzer\Microsoft.IO.RecyclableMemoryStream.dll "%TEMP%"
 del packages\linux-x64\synchronizzer\web.config packages\linux-x64\synchronizzer\*.deps.json packages\linux-x64\synchronizzer\Microsoft.*.dll
