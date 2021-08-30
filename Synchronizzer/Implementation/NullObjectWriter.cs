4ebf43829edf57bcefcd75355cddee019cfaaed9
Aristarkh Zagorodnikov <xm@drive.net>
2021-08-30 14:00:01 +0300

    Add NullObjectWriter

---
+++ Synchronizzer/Implementation/NullObjectWriter.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Synchronizzer.Implementation
+{
+    internal sealed class NullObjectWriter : IObjectWriter
+    {
+        public static NullObjectWriter Instance { get; } = new();
+
+        public Task Delete(string objectName, CancellationToken cancellationToken) => Task.CompletedTask;
+
+        public Task Upload(string objectName, Stream readOnlyInput, CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
