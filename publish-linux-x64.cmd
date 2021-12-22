a238890489014a9255466ff741ddc1c37eecb77e
Aristarkh Zagorodnikov <xm@drive.net>
2021-12-22 08:55:16 +0300

    Revert "Simplify build"
    This reverts commit 5e845befec2ab1e94038eecda2a6cba136d7c689.

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -2,4 +2,8 @@
 rmdir /s /q packages\linux-x64\synchronizzer
 mkdir packages\linux-x64\synchronizzer
 dotnet publish Synchronizzer --force --output packages\linux-x64\synchronizzer -c Integration -r linux-x64 --self-contained false
-del packages\linux-x64\synchronizzer\web.config packages\linux-x64\synchronizzer\*.deps.json
+move packages\linux-x64\synchronizzer\Microsoft.Extensions.Hosting.Systemd.dll "%TEMP%"
+move packages\linux-x64\synchronizzer\Microsoft.IO.RecyclableMemoryStream.dll "%TEMP%"
+del packages\linux-x64\synchronizzer\web.config packages\linux-x64\synchronizzer\*.deps.json packages\linux-x64\synchronizzer\Microsoft.*.dll
+move "%TEMP%\Microsoft.Extensions.Hosting.Systemd.dll" packages\linux-x64\synchronizzer
+move "%TEMP%\Microsoft.IO.RecyclableMemoryStream.dll" packages\linux-x64\synchronizzer
