0a32a1ba2c6a6b2e18eadf4ca31e2ac031600beb
Aristarkh Zagorodnikov <xm@x-infinity.com>
2022-04-18 19:36:56 +0300

    Add single-file deployment

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,9 +1,8 @@
 @echo off
 rmdir /s /q packages\linux-x64\synchronizzer
 mkdir packages\linux-x64\synchronizzer
-dotnet publish Synchronizzer --force --output packages\linux-x64\synchronizzer -c Integration -r linux-x64 --self-contained false
-move packages\linux-x64\synchronizzer\Microsoft.Extensions.Hosting.Systemd.dll "%TEMP%"
-move packages\linux-x64\synchronizzer\Microsoft.IO.RecyclableMemoryStream.dll "%TEMP%"
-del packages\linux-x64\synchronizzer\web.config packages\linux-x64\synchronizzer\*.deps.json packages\linux-x64\synchronizzer\Microsoft.*.dll
-move "%TEMP%\Microsoft.Extensions.Hosting.Systemd.dll" packages\linux-x64\synchronizzer
-move "%TEMP%\Microsoft.IO.RecyclableMemoryStream.dll" packages\linux-x64\synchronizzer
+dotnet publish Synchronizzer --force --output packages\linux-x64\synchronizzer -c Integration -r linux-x64 --no-self-contained
+del packages\linux-x64\synchronizzer\*.deps.json
+rmdir /s /q packages\linux-x64\synchronizzer-single
+mkdir packages\linux-x64\synchronizzer-single
+dotnet publish Synchronizzer --force --output packages\linux-x64\synchronizzer-single -c Integration -r linux-x64 --no-self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true
