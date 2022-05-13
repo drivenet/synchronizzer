c3079ce789f4f9a20e2264f75926ec7ab62a14e2
Aristarkh Zagorodnikov <xm@x-infinity.com>
2022-05-13 18:35:52 +0300

    Add ObjectInfo.FindObjectByMetadata

--- Synchronizzer/Implementation/ObjectInfos.cs
+++ Synchronizzer/Implementation/ObjectInfos.cs
@@ -113,20 +113,30 @@ namespace Synchronizzer.Implementation
             ++_skip;
         }
 
-        public bool HasFreshObject(ObjectInfo objectInfo)
+        public ObjectInfo? FindObjectByMetadata(ObjectInfo objectInfo)
         {
             if (_infos is null)
             {
-                return false;
+                return null;
             }
 
             var index = _infos.BinarySearch(objectInfo, MetadataComparer);
             if (index < 0)
             {
+                return null;
+            }
+
+            return _infos[index];
+        }
+
+        public bool HasFreshObject(ObjectInfo objectInfo)
+        {
+            if (FindObjectByMetadata(objectInfo) is not { } foundInfo)
+            {
                 return false;
             }
 
-            return _infos[index].Timestamp >= objectInfo.Timestamp;
+            return foundInfo.Timestamp >= objectInfo.Timestamp;
         }
 
         public bool HasObjectByName(ObjectInfo objectInfo)
