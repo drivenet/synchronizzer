23649ac89921d25c14d5ff030b55604ea9e6f09c
Aristarkh Zagorodnikov <xm@x-infinity.com>
2023-11-09 13:19:04 +0300

    feat(FilesystemObjectReader/Writer): open files in async mode

--- Synchronizzer/Implementation/FilesystemObjectReader.cs
+++ Synchronizzer/Implementation/FilesystemObjectReader.cs
@@ -14,16 +14,22 @@ namespace Synchronizzer.Implementation
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously -- opening local file is synchronous
         public async Task<ReadObject?> Read(string objectName, CancellationToken cancellationToken)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             var path = FilesystemUtils.PreparePath(objectName, _context);
             cancellationToken.ThrowIfCancellationRequested();
             try
             {
-                var file = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read | FileShare.Delete);
-                return new ReadObject(file, file.Length);
+                var file = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read | FileShare.Delete, sizeof(long), true);
+                try
+                {
+                    return new ReadObject(file, file.Length);
+                }
+                catch
+                {
+                    await file.DisposeAsync();
+                    throw;
+                }
             }
             catch (DirectoryNotFoundException)
             {
