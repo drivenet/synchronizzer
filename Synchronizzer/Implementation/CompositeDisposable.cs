974cc0ee7d2cd88e6f14a0cc44d9169dee90d483
Aristarkh Zagorodnikov <xm@x-infinity.com>
2025-03-24 23:26:06 +0000

    feat: add CompositeDisposable

---
+++ Synchronizzer/Implementation/CompositeDisposable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Synchronizzer.Implementation;
+
+internal sealed class CompositeDisposable : IDisposable
+{
+    private readonly IEnumerable<IDisposable> _disposables;
+
+    public CompositeDisposable(IEnumerable<IDisposable> disposables)
+    {
+        _disposables = disposables ?? throw new ArgumentNullException(nameof(disposables));
+    }
+
+    public void Dispose()
+    {
+        foreach (var disposable in _disposables)
+        {
+            disposable.Dispose();
+        }
+    }
+}
